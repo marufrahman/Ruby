5ae191bf364c479aaa1c096b6e5660ce0cdadb6b
Maruf Rahman <marufrahman@berkeley.edu>
2016-10-05 01:42:30 -0700

    del hw3

--- hw3/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'students#new'
-  get '/teachers/new', to: 'teachers#new'
-  post '/teachers', to: 'teachers#create'
-  get '/students/new', to: 'students#new'
-  post '/students', to: 'students#create'
-end
