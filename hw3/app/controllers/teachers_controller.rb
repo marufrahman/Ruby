809a875fcbe0c33f82fca07af225d008378bc0c3
Maruf Rahman <marufrahman@berkeley.edu>
2016-10-05 01:39:59 -0700

    finised hw3

---
+++ hw3/app/controllers/teachers_controller.rb
@@ -0,0 +1,13 @@
+class TeachersController < ApplicationController
+  def new
+    @placeholder_course = '186'
+  end
+
+  def create
+    # Hint: params??
+    @full_name = params[:full_name]
+    @course = params[:course]
+    @grade_level = params[:grade_level]
+    render 'show'
+  end
+end
